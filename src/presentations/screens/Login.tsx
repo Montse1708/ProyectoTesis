49fdf633d619a6d8c573dbec366bf6c704ee5ea5
Jessica Morales <88988059+Montse1708@users.noreply.github.com>
2024-11-14 20:02:04 -0600

    Add src file

---
+++ src/presentations/screens/Login.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
+export const Login = () => {
+  return (
+    <LinearGradient colors={['#2c3e50', '#bdc3c7']} style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Login</Text>
+        <Text style={styles.subtitle}>Sign In to your account</Text>
+        <TextInput
+          placeholder="nombre@gmail.com"
+          placeholderTextColor={'gray'}
+          style={styles.TextInput}
+        />
+        <TextInput
+          placeholder="Contraseña"
+          placeholderTextColor={'gray'}
+          style={styles.TextInput}
+        />
+      </View>
+    </LinearGradient>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent', 
+  },
+  title: {
+    fontSize: 50,
+    textAlign: 'center',
+    color: '#34434D',
+    padding: 20,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: 30,
+    color: 'gray',
+  },
+  TextInput: {
+    borderColor: 'gray',
+    padding: 10,
+    paddingStart: 20,
+    width: '80%',
+    color: 'gray',
+    marginTop: 20,
+    height: 50,
+    borderRadius: 30,
+    backgroundColor: '#FFFF',
+  },
+});
