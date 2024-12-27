28faef440a9b85ba148fe9918296f510ad095dea
Jessica Montserrat Morales Enrique <jessica.morales5556@alumnos.udg.mx>
2024-12-27 01:24:39 -0600

    Add login desing

--- src/presentation/screens/Login.tsx
+++ src/presentation/screens/Login.tsx
@@ -11,60 +11,79 @@ export const Login = () => {
       <Image
         source={require('../../assets/fondo.jpg') } style={styles.backgroundImage}
       />
-      <Text style={styles.text}>Login</Text>
-      <TextInput style={styles.email} placeholder="Email" />
-      <TextInput style={styles.password} placeholder="Contraseña" />
-      <Pressable onPress={() => navigation.navigate('HomeScreen' as never)}>
-        <Text style={styles.button}>Iniciar sesión</Text>
-      </Pressable>
+      
+      <View style={styles.formContainer}>
+        <Text style={styles.text}>Login</Text>
+        <TextInput style={styles.email} placeholder="Email" />
+        <TextInput style={styles.password} placeholder="Contraseña" />
+        <Pressable onPress={() => navigation.navigate('HomeScreen' as never)}>
+          <Text style={styles.button}>Iniciar sesión</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     justifyContent: 'flex-start',
     alignItems: 'center',
-    backgroundColor: '#f5f5f5',
-    textShadowColor: 'white',
-    flex: 1, 
     paddingHorizontal: 20,
+    backgroundColor: '#f5f5f5',
+    position: 'relative', 
+  },
+  backgroundImage: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: width, 
+    height: height,
+    resizeMode: 'cover', 
+  },
+  formContainer: {
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    width: width,
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    marginTop: '30%', // Ajuste para que no se sobreponga tanto en la pantalla
+    height: height * 0.7, // Establecer un alto proporcional al 70% de la pantalla
+    borderRadius: 100, // Valor más pequeño para bordes redondeados
+    position: 'absolute', // Para asegurarse de que esté encima de la imagen de fondo
+    top: '15%', // Para desplazarlo un poco más abajo si es necesario
   },
   text: {
     fontSize: 50,
     fontWeight: 'bold',
-    marginTop: 100,
-    color: 'white'
+    color: '#e8ba61',
+    marginTop: 80
   },
   email: {
     fontSize: 20,
-    padding: 15, 
-    backgroundColor: 'lightblue',
+    padding: 10,
+    backgroundColor: '#f3ebdf',
     borderRadius: 10,
-    width: '80%',
     marginTop: 40,
+    width: '80%', // Asegura que el campo ocupe todo el ancho disponible
+    textAlign: 'left', // Alineación del texto a la izquierda
   },
   password: {
     fontSize: 20,
-    padding: 15, 
-    backgroundColor: 'lightblue',
-    borderRadius: 10,
-    width: '80%',
-    marginTop: 40,
-  },
-  button:{
-    marginTop: 40,
-    fontSize: 20,
-    backgroundColor: 'violet',
     padding: 10,
+    backgroundColor: '#f3ebdf',
     borderRadius: 10,
-    width: '100%',
-    margin: 10
+    marginTop: 20,
+    width: '80%', // Asegura que el campo ocupe todo el ancho disponible
+    textAlign: 'left', // Alineación del texto a la izquierda
   },
-  backgroundImage:{
-      position: 'absolute',
-      width: width,
-      height: height
-    },
+  button: {
+    fontSize: 20,
+    color: 'white',
+    backgroundColor: '#253547',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 30,
+  },
 });
 
